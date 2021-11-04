9c220cfca2dba4ae30658c6620fa50a71aba3d4a
mgorkii <maksim.gorkii@nlplogix.com>
2021-11-04 15:39:08 -0400

    fix vm image

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -11,7 +11,7 @@ resource "azurerm_network_interface" "test" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
+resource "azurerm_linux_virtual_machine" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
   resource_group_name = var.resource_group
